8ada1a7ebaa7150fbe2c5918f6b19d6a3de659e4
ZioNEnes <cngz.enes2005@gmail.com>
2025-03-20 15:08:57 +0300

    NamespaceKullanimiVeGenisletilebilirlik

---
+++ NamespaceKullanimiVeGenisletilebilirlik/Hesaplama.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+//Namespace, C#’ta sınıfları, arayüzleri, yapılandırmaları ve diğer bileşenleri gruplamak için kullanılan bir yapıdır. 
+//Büyük projelerde isim çakışmalarını önlemek ve kodun düzenli olmasını sağlamak için kullanılır.
+
+//Hesaplama sınıfını farklı bir namespace içinde tanımlıyoruz
+namespace Matematik
+{
+    public class Hesaplama
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+    }
+
+}
---
+++ NamespaceKullanimiVeGenisletilebilirlik/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Matematik;
+namespace NamespaceKullanimiVeGenisletilebilirlik
+{
+    public class Test
+    {
+         static void Main()
+        {
+            Hesaplama hesap = new Hesaplama();
+            Console.WriteLine("Toplam: " + hesap.Topla(5, 7));
+            Console.WriteLine("Çarpım: " + hesap.Carp(4, 6));
+        }
+    }
+}
