ca33922027ae9cefb1697f0ae50d5fe07714758f
ZioNEnes <cngz.enes2005@gmail.com>
2025-03-20 15:07:58 +0300

    ErisimBelirleyicilerVeDegiskenlerİleCalisma

---
+++ ErisimBelirleyicilerVeDegiskenlerİleCalisma/Program.cs
@@ -0,0 +1,9 @@
+﻿using ErisimBelirleyicilerVeDegiskenlerİleCalisma;
+
+User user = new User();
+
+user.Email = "cngz.enes2005@gmail.com";
+user.SetAdSoyad("EnesCengiz");
+user.SetYas(20);
+user.BilgileriGoster();
+
---
+++ ErisimBelirleyicilerVeDegiskenlerİleCalisma/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirleyicilerVeDegiskenlerİleCalisma
+{
+    public class User
+    {
+        private string adSoyad;
+        private int yas;
+        public string Email;
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+        public void SetAdSoyad(string _adSoyad)
+        {
+            adSoyad = _adSoyad;
+        }
+        public int GetYas()
+        {
+            return yas;
+        }
+        public void SetYas(int _yas)
+        {
+                yas = _yas;
+        }
+        public void BilgileriGoster()
+        {
+            Console.WriteLine("----- Kullanıcı Bilgileri -----");
+            Console.WriteLine("Ad Soyad:"+adSoyad);
+            Console.WriteLine("Yaş:"+yas);
+            Console.WriteLine("Email:"+Email);
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
