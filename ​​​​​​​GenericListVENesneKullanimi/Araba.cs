6540e1728c4335497733aec3932144268db2b51f
ZioNEnes <cngz.enes2005@gmail.com>
2025-04-08 14:40:05 +0300

    ​​​​​​​GenericListVENesneKullanimi

---
+++ ​​​​​​​GenericListVENesneKullanimi/Araba.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _​​​​​​​GenericListVENesneKullanimi
+{
+    public class Araba
+    {
+        public string Marka;
+        public string Model;
+        public double BenzinHarcamasi; // 100 km başına harcanan yakıt
+        public double ToplamMesafe;  // Toplam mesafe
+
+        public Araba(string marka, string model, double benzinHarcamasi, double toplamMesafe)
+        {
+            Marka = marka;
+            Model = model;
+            BenzinHarcamasi = benzinHarcamasi;
+           ToplamMesafe = toplamMesafe;
+        }
+
+        public double ToplamBenzinHesapla()
+        {
+            return (ToplamMesafe / 100) * BenzinHarcamasi;
+        }
+
+        public void BilgiYazdir()
+        {
+            Console.WriteLine($"Marka: {Marka}, Model: {Model}");
+            Console.WriteLine($"Toplam Mesafe: {ToplamMesafe} km");
+            Console.WriteLine($"100 km'de Harcanan Yakıt: {BenzinHarcamasi} L");
+            Console.WriteLine($"Toplam Harcanan Yakıt: {ToplamBenzinHesapla()} L\n");
+        }
+    }
+}
---
+++ ​​​​​​​GenericListVENesneKullanimi/Program.cs
@@ -0,0 +1,17 @@
+﻿using _GenericListVENesneKullanimi;
+// Üç farklı araba nesnesi oluşturuldu.
+Araba araba1 = new Araba("Bmw", "X5", 9.0, 65);
+Araba araba2 = new Araba("Mercedes", "C200", 8.2, 65);
+Araba araba3 = new Araba("Toyota", "Corolla", 6.5, 65);
+
+// Araba nesneleri listeye eklendi.
+List<Araba> arabaListesi = new List<Araba>();
+arabaListesi.Add(araba1);
+arabaListesi.Add(araba2);
+arabaListesi.Add(araba3);
+
+foreach (var araba in arabaListesi)
+ {
+     araba.BilgiYazdir();
+ }
+
